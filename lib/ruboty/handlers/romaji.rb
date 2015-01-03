3c9c9815e9fd5a76d1372bffa2283944647557b9
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-03 18:23:32 +0900

    add dependency gems

--- lib/ruboty/handlers/romaji.rb
+++ lib/ruboty/handlers/romaji.rb
@@ -1,14 +1,16 @@
+require 'romkan'
+
 module Ruboty
   module Handlers
     class Romaji < Base
       on /to_kunrei (?<keyword>.+)/,  name: "to_kunrei",  description: "convert keyword to kunrei"
       on /to_hepburn (?<keyword>.+)/, name: "to_hepburn", description: "convert keyword to hepburn"
       def to_kunrei(message)
-        @message[:keyword].to_roma.to_kunrei
+        message.reply(message[:keyword].to_roma.to_kunrei)
       end
 
       def to_hepburn(message)
-        @message[:keyword].to_roma.to_hepburn
+        message.reply(message[:keyword].to_roma.to_hepburn)
       end
     end
   end
